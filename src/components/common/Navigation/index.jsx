c7f5628b56746e6fab3ff2b2bbb1809bfad3d971
Yuchen <happyzychjl@gmail.com>
2019-05-21 22:26:45 +0800

    first commit

---
+++ src/components/common/Navigation/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./style.scss"
+
+class Navigation extends React.Component{
+
+    render() {
+        return (
+
+        )
+    }
+}
+
+export default Navigation;
